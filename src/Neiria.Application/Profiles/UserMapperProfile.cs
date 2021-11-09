42cfe421d9b1423ed9a7e0ee5991947531e1935c
Leo H <leoh1992@live.nl>
2021-11-09 17:41:10 +0100

    Created User information in project
    - add repo, model, viewmodel and controller in project
    - create a new migration with ef core
    - change length of description in catergory model

---
+++ src/Neiria.Application/Profiles/UserMapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Neiria.Domain.Models;
+using Neiria.Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neiria.Application.Profiles
+{
+  public class UserMapperProfile : Profile
+  {
+    public UserMapperProfile()
+    {
+      CreateMap<User, UserViewModel>();
+      CreateMap<UserViewModel, User>();
+    }
+  }
+}
